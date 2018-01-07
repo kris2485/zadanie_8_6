82f921dfaec60e4bb8c90894d4a612ba4d178370
Krzysztof Rajnert <krzysiekrajnert@gmail.com>
2018-01-07 18:57:19 +0100

    Edit - create 1 if in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@ var a = 2;
 var b = 4;
 var value = (a * a) - (2 * a * b) + (b * b);
 if (value > 0) {
- console.log("wynik dodatni");
+	console.log("wynik dodatni");
 }
-else if (value < 0) {
- console.log("wynik ujemny");
+ else if (value < 0) {
+ 	console.log("wynik ujemny");
 }
-if (value != 0) {
- console.log("wynik jest inny niż 0");
+ else if (value != 0) {
+  	console.log("wynik jest inny niż 0");
 }
-if (value === 0) {
- console.log("wynik jest równy 0");
+ else if (value === 0) {
+ 	console.log("wynik jest równy 0");
 }
