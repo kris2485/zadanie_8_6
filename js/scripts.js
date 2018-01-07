6b467f092015e0df44e4208a73b1459f6755300c
Krzysztof Rajnert <krzysiekrajnert@gmail.com>
2018-01-07 15:46:10 +0100

    Add files to repository

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// scripts.js
+var a = 2;
+var b = 4;
+var value = (a * a) - (2 * a * b) + (b * b);
+if (value > 0) {
+ console.log("wynik dodatni");
+}
+else if (value < 0) {
+ console.log("wynik ujemny");
+}
+if (value != 0) {
+ console.log("wynik jest inny niż 0")
+}
