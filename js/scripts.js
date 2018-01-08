6d76560785bc5e5b64ee0fa917a5fcc20472c251
Krzysztof Rajnert <krzysiekrajnert@gmail.com>
2018-01-08 14:14:49 +0100

    Edit scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,9 @@ var value = (a * a) - (2 * a * b) + (b * b);
 if (value > 0) {
 	console.log("wynik dodatni");
 }
- else if (value < 0) {
+ else (value < 0) {
  	console.log("wynik ujemny");
 }
- else if (value != 0) {
+ else (value != 0) {
   	console.log("wynik jest inny niż 0");
-}
- else if (value === 0) {
- 	console.log("wynik jest równy 0");
 }
